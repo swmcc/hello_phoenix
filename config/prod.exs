4573bfd63f511c49e04390f46f385cdc7e74999c
Stephen McCullough <stephen@skillfulgorilla.com>
2016-11-20 16:42:56 +0000

    Made app deployable to heroku
    Now uses production config from Heroku ENV variables and decrease socket
    timeout.

--- config/prod.exs
+++ config/prod.exs
@@ -14,10 +14,19 @@ use Mix.Config
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "fast-castle-14913.herokuapp.com.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -55,7 +64,3 @@ config :logger, level: :info
 #
 #     config :hello_phoenix, HelloPhoenix.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
